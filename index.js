30a373ff4700093af32f6e487913426ef2916f0d
Bebli Carl <beblicarl.cb@gmail.com>
2021-03-24 19:03:08 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
+
+var typed = new Typed('.type', {
+    // Waits 1000ms after typing "First"
+    strings: ['Fashion designer.', 'Philanthropist.','Public Speaker','Consultant','Business Woman'],
+    typeSpeed: 100,
+    backSpeed: 70,
+    loop: true,
+  });
